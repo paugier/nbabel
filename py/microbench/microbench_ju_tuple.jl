cb93525a53ab124e6367b746b698c4b6169bd9c7
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-01-19 21:37:04 +0100

    Better microbench comparison Julia Pythran

--- py/microbench/microbench_ju_tuple.jl
+++ py/microbench/microbench_ju_tuple.jl
@@ -5,8 +5,9 @@ using BenchmarkTools
 
 function compute_acceleration!(pos, mass, acc)
     N = length(pos)
+
     @inbounds for i in 1:N
-        acc[i] = (0.0, 0.0, 0.0, 0.0)
+        acc[i] = zero(eltype(acc))
     end
 
     @inbounds for i in 1:N
@@ -28,12 +29,16 @@ function compute_acceleration!(pos, mass, acc)
     return acc
 end
 
-function main()
+import Base: zero
+T4f = Tuple{Float64, Float64, Float64, Float64}
+zero(T::Type{T4f}) = (0., 0., 0., 0.)
 
+T3f = Tuple{Float64, Float64, Float64}
+zero(T::Type{T3f}) = (0., 0., 0.)
+
+
+function main4()
     number_particles = 1024
-
-    T4f = Tuple{Float64, Float64, Float64, Float64}
-
     masses = Vector{Float64}(undef, number_particles)
     positions = Vector{T4f}(undef, number_particles)
     accelerations = Vector{T4f}(undef, number_particles)
@@ -46,7 +51,26 @@ function main()
     end
 
     @btime compute_acceleration!($accelerations, $masses, $positions)
-
 end
 
-main()
+
+function main3()
+    number_particles = 1024
+    masses = Vector{Float64}(undef, number_particles)
+    positions = Vector{T3f}(undef, number_particles)
+    accelerations = Vector{T3f}(undef, number_particles)
+
+    x = 0.0
+    for i in eachindex(accelerations)
+        positions[i] = (x, 0., 0.)
+        x += 1.0
+        accelerations[i] = (0., 0., 0.)
+    end
+
+    @btime compute_acceleration!($accelerations, $masses, $positions)
+end
+
+print("With Tuple{Float64, Float64, Float64, Float64}:\n")
+main4()
+print("With Tuple{Float64, Float64, Float64}:\n")
+main3()
--- py/microbench/microbench_pythran.py
+++ py/microbench/microbench_pythran.py
@@ -4,45 +4,47 @@ import numpy as np
 from transonic import boost
 from transonic.util import timeit_verbose as timeit
 
+dim = 3
 
 @boost
-def compute(accelerations: "float[:,3]", masses: "float[:]", positions: "float[:,3]"):
+def compute(accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"):
     nb_particules = masses.size
-    vector = np.empty(3)
+    vector = np.empty(dim)
     for index_p0 in range(nb_particules - 1):
         position0 = positions[index_p0]
         mass0 = masses[index_p0]
         for index_p1 in range(index_p0 + 1, nb_particules):
             mass1 = masses[index_p1]
-            for i in range(3):
+            for i in range(dim):
                 vector[i] = position0[i] - positions[index_p1, i]
             distance = sqrt(sum(vector ** 2))
-            coef = 1.0 / distance ** 3
-            for i in range(3):
+            coef = 1.0 / distance ** dim
+            for i in range(dim):
                 accelerations[index_p0, i] -= coef * mass1 * vector[i]
                 accelerations[index_p1, i] += coef * mass0 * vector[i]
 
 
 @boost
-def compute2(accelerations: "float[:,3]", masses: "float[:]", positions: "float[:,3]"):
+def compute_opt(accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"):
     nb_particules = masses.size
-    vector = np.empty(3)
+    vector = np.empty(dim)
     for index_p0 in range(nb_particules - 1):
         position0 = positions[index_p0]
         mass0 = masses[index_p0]
         for index_p1 in range(index_p0 + 1, nb_particules):
-            for i in range(3):
+            for i in range(dim):
                 vector[i] = position0[i] - positions[index_p1, i]
             distance2 = sum(vector ** 2)
             distance3 = distance2 * sqrt(distance2)
             coef1 = masses[index_p1] / distance3
             coef0 = mass0 / distance3
-            for i in range(3):
+            for i in range(dim):
                 accelerations[index_p0, i] -= coef1 * vector[i]
                 accelerations[index_p1, i] += coef0 * vector[i]
 
 
-shape = 1024, 3
+shape = 1024, dim
+print("shape=", shape)
 
 masses = np.zeros(shape[0])
 positions = np.zeros(shape)
@@ -54,7 +56,14 @@ for ip in range(shape[0]):
     positions[ip, 0] = x
     x += 1.0
 
-
 glo = globals()
 norm = timeit("compute(accelerations, masses, positions)", globals=glo)
-timeit("compute2(accelerations, masses, positions)", globals=glo, norm=norm)
+timeit("compute_opt(accelerations, masses, positions)", globals=glo, norm=norm)
+
+"""
+Oh, `pythran -P` gives:
+```
+coef = (1.0 / (np.square(math.sqrt(builtins.sum(np.square(vector)))) * math.sqrt(builtins.sum(np.square(vector)))))
+```
+I guess the C++ compiler is smart enough to simplify that, but it's a bit strange...
+"""
