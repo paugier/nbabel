69922f42b8572c0c290e1d175e7619b83161b9e0
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-01-24 21:20:58 +0100

    Better figures power

--- power/load_data.py
+++ power/load_data.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import h5py
 from scipy.integrate import trapz
+
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 here = Path(__file__).absolute().parent
 
 dir_saved = here / "saved"
 
-nb_particles_short = "2k"
+nb_particles_short = "16k"
 
 paths_h5 = sorted(dir_saved.glob(f"{nb_particles_short}_*.h5"))
 
@@ -37,8 +39,8 @@ power_sleep = np.percentile(watts, 5)
 nb_cores = 12
 power_sleep_1core = power_sleep / nb_cores
 
-# fig, ax = plt.subplots()
-# ax.plot(times, watts)
+# fig, ax0 = plt.subplots()
+# ax0.plot(times, watts)
 
 consommations = np.empty(df.shape[0])
 
@@ -49,8 +51,8 @@ for index, row in df.iterrows():
         times < row.timestamp_end + deltat
     )
 
-    # ax.vlines(row.timestamp_start, 0, 250, colors="g")
-    # ax.vlines(row.timestamp_end, 0, 250, colors="r")
+    # ax0.vlines(row.timestamp_start, 0, 250, colors="g")
+    # ax0.vlines(row.timestamp_end, 0, 250, colors="r")
 
     times_run = times[cond]
     watts_run = watts[cond]
@@ -65,7 +67,7 @@ for index, row in df.iterrows():
 df["consommation"] = consommations
 df["power"] = df["consommation"] / df["elapsed_time"]
 
-print(df)
+# print(df)
 
 columns = [
     "implementation",
@@ -107,22 +109,55 @@ df_out["CO2_alt"] = compute_CO2_mass(df_out["consommation_alt"])
 df_out["CO2_core"] = compute_CO2_mass(df_out["consommation_core"])
 
 
-fig, ax = plt.subplots()
+fig, ax0 = plt.subplots()
 fig, ax1 = plt.subplots()
 fig, ax2 = plt.subplots()
 
+axes = [ax0, ax1, ax2]
+quantities = ["CO2", "CO2_core", "CO2_alt"]
+
 for index, row in df_out.iterrows():
+    implementation, language = row.name
 
-    marker = "o"
-    color = "b"
-    if row.name[1] == "py":
-        marker = "s"
+    if language == "py":
         color = "r"
-    elif row.name[1] in ("cpp", "fortran"):
+    elif language in ("cpp", "fortran"):
         color = "g"
+    elif language == "julia":
+        color = "b"
+    else:
+        color = "y"
 
-    factor_time = 0.93
-    factor_cons = 1.03
+    if (
+        language == "julia"
+        or "jit" in implementation
+        or "pypy" in implementation
+        or "numba" in implementation
+    ):
+        marker = "o"
+        markersize = "8"
+    else:
+        marker = "*"
+        markersize = "10"
+
+    for ax, quantity in zip(axes, quantities):
+        ax.plot(
+            row.elapsed_time,
+            row[quantity],
+            marker=marker,
+            color=color,
+            markersize=markersize,
+        )
+
+x_ratios = []
+y_ratios = []
+for ax in axes:
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    x_ratios.append(xmax / xmin)
+    y_ratios.append(ymax / ymin)
+
+for index, row in df_out.iterrows():
 
     name = row.name[0].capitalize()
     if name.endswith("Pythran high-level jit"):
@@ -130,30 +165,111 @@ for index, row in df_out.iterrows():
     elif name == "Pypy":
         name = "PyPy"
 
-    if name == "Numba":
-        factor_cons = 0.94
-    elif name == "Pythran\n naive":
-        factor_cons = 0.85
+    factor_time = -0.008
+    factor_cons = 0.005
 
-    ax.plot(row.elapsed_time, row.CO2, marker=marker, color=color)
-    ax.text(factor_time * row.elapsed_time, factor_cons * row.CO2, name)
+    if nb_particles_short == "16k":
+        if name == "PyPy":
+            factor_cons = -0.003
+            factor_time = -0.018
 
-    ax1.plot(row.elapsed_time, row.CO2_core, marker=marker, color=color)
-    ax1.text(factor_time * row.elapsed_time, factor_cons * row.CO2_core, name)
+        elif name == "Pythran\n naive":
+            factor_time = -0.01
+            factor_cons = -0.020
 
-    ax2.plot(row.elapsed_time, row.CO2_alt, marker=marker, color=color)
-    ax2.text(factor_time * row.elapsed_time, factor_cons * row.CO2_alt, name)
+        elif name.startswith("Fortran"):
+            factor_cons = -0.01
+
+        elif name.startswith("Julia nbabel"):
+            factor_time = 0.005
+            factor_cons = -0.004
+
+    elif nb_particles_short == "1k":
+
+        if name == "Numba":
+            factor_time = 0.006
+            factor_cons = -0.006
+
+        if name == "Pythran\n naive":
+            factor_time = -0.01
+            factor_cons = -0.022
+
+    for iax, (ax, quantity) in enumerate(zip(axes, quantities)):
+        ax.text(
+            row.elapsed_time * (1 + factor_time * x_ratios[iax]),
+            row[quantity] * (1 + factor_cons * y_ratios[iax]),
+            name,
+        )
 
 
-ax.set_ylabel("Production CO2 full node during run (kg)")
-ax1.set_ylabel("Production CO2 used core(s) during run (kg)")
-ax2.set_ylabel("Production CO2 full node during time slowest run (kg)")
+ax0.set_ylabel("Production CO$_2$ full node during run (kg)")
+ax1.set_ylabel("Production CO$_2$ used core(s) during run (kg)")
+ax2.set_ylabel("Production CO$_2$ full node during time slowest run (kg)")
 
-for _ in (ax, ax1, ax2):
+for _ in (ax0, ax1, ax2):
     _.set_xscale("log")
     _.set_yscale("log")
     _.set_xlabel("Elapsed time (s)")
     _.figure.tight_layout()
+
+
+ax0.legend(
+    handles=[
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            markerfacecolor="r",
+            marker="s",
+            markersize=8,
+            label="Python",
+        ),
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            marker="o",
+            markerfacecolor="b",
+            markeredgecolor="b",
+            markersize=8,
+            label="Julia",
+        ),
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            marker="*",
+            markerfacecolor="g",
+            markeredgecolor="g",
+            markersize=10,
+            label="C++ & Fortran",
+        ),
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            marker="o",
+            markerfacecolor="w",
+            markeredgecolor="k",
+            markersize=8,
+            label="JIT compilation",
+        ),
+        Line2D(
+            [0],
+            [0],
+            color="w",
+            marker="*",
+            markerfacecolor="w",
+            markeredgecolor="k",
+            markersize=10,
+            label="AOT compilation",
+        ),
+    ]
+)
+
+# plt.close(ax0.figure)
+plt.close(ax1.figure)
+plt.close(ax2.figure)
 
 
 plt.show()
