ff4f653167ff3491c0151a282427b56edd0ab322
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-01-25 21:29:27 +0100

    Try other Pythran implementations

--- power/README.md
+++ power/README.md
@@ -8,5 +8,8 @@ oarsub -p "cluster='taurus'" -I
 
 Benchmarks run in Taurus node at Grid 5000:
 
-2 CPUs Intel Xeon E5-2630, 6 cores/CPU, 32GB RAM, 557GB HDD, 1 x 10Gb Ethernet
+2.30 GHz Intel Xeon E5-2630, node with 2 CPUs, 6 cores/CPU, 32GB RAM, 557GB HDD, 1 x 10Gb Ethernet
 
+Compared to Zwart (2020):
+
+2.7 GHz Intel Xeon E-2176M
--- py/microbench/microbench_pythran.py
+++ py/microbench/microbench_pythran.py
@@ -6,8 +6,11 @@ from transonic.util import timeit_verbose as timeit
 
 dim = 3
 
+
 @boost
-def compute(accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"):
+def compute(
+    accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"
+):
     nb_particules = masses.size
     vector = np.empty(dim)
     for index_p0 in range(nb_particules - 1):
@@ -25,7 +28,9 @@ def compute(accelerations: "float[:,:]", masses: "float[:]", positions: "float[:
 
 
 @boost
-def compute_opt(accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"):
+def compute_opt(
+    accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"
+):
     nb_particules = masses.size
     vector = np.empty(dim)
     for index_p0 in range(nb_particules - 1):
@@ -41,6 +46,52 @@ def compute_opt(accelerations: "float[:,:]", masses: "float[:]", positions: "flo
             for i in range(dim):
                 accelerations[index_p0, i] -= coef1 * vector[i]
                 accelerations[index_p1, i] += coef0 * vector[i]
+
+
+@boost
+def compute_opt1(
+    accelerations: "float[:,3]", masses: "float[:]", positions: "float[:,3]"
+):
+    nb_particules = masses.size
+    vector = np.empty_like(positions[0])
+    acc0 = np.empty_like(accelerations[0])
+    for index_p0 in range(nb_particules - 1):
+        np.copyto(acc0, accelerations[index_p0])
+        for index_p1 in range(index_p0 + 1, nb_particules):
+            np.copyto(vector, positions[index_p0] - positions[index_p1])
+            distance = sqrt(np.sum(vector ** 2))
+            coef = np.power(distance, -3)
+            np.copyto(acc0, acc0 + coef * masses[index_p1] * vector)
+            accelerations[index_p1] += coef * masses[index_p0] * vector
+        accelerations[index_p0] = -acc0
+
+
+@boost
+def compute_opt2(
+    accelerations: "float[:,:]", masses: "float[:]", positions: "float[:,:]"
+):
+    nb_particules = masses.size
+    vector = np.empty(dim)
+    accs = np.empty(dim)
+    for index_p0 in range(nb_particules - 1):
+        position0 = positions[index_p0]
+        mass0 = masses[index_p0]
+        accs.fill(0)
+        for index_p1 in range(index_p0 + 1, nb_particules):
+            for i in range(dim):
+                vector[i] = position0[i] - positions[index_p1, i]
+            d2 = 0.0
+            for i in range(dim):
+                d2 = d2 + vector[i] * vector[i]
+            distance3 = d2 * sqrt(d2)
+            coef1 = masses[index_p1] / distance3
+            coef0 = mass0 / distance3
+            for i in range(dim):
+                accs[i] = accs[i] + coef1 * vector[i]
+                accelerations[index_p1, i] = (
+                    accelerations[index_p1, i] + coef0 * vector[i]
+                )
+        accelerations[index_p0] -= accs
 
 
 shape = 1024, dim
@@ -59,6 +110,10 @@ for ip in range(shape[0]):
 glo = globals()
 norm = timeit("compute(accelerations, masses, positions)", globals=glo)
 timeit("compute_opt(accelerations, masses, positions)", globals=glo, norm=norm)
+
+timeit("compute_opt1(accelerations, masses, positions)", globals=glo, norm=norm)
+timeit("compute_opt2(accelerations, masses, positions)", globals=glo, norm=norm)
+
 
 """
 Oh, `pythran -P` gives:
