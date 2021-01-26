5faf8b5f9d5a49825f75ee748a5ea920ae816ad9
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-01-26 11:48:42 +0100

    power: make_fig...

--- power/make_fig.py
+++ power/make_fig.py
@@ -1,13 +1,10 @@
 from pathlib import Path
 from pprint import pprint
 
-import numpy as np
-import pandas as pd
-import h5py
-from scipy.integrate import trapz
-
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+
+from util import load_data, complete_df_out
 
 here = Path(__file__).absolute().parent
 
@@ -20,56 +17,10 @@ pprint(paths_h5)
 
 path_h5 = paths_h5[0]
 
-path_csv = path_h5.with_suffix(".csv")
-
-assert path_csv.exists()
-assert path_h5.exists()
-
-df = pd.read_csv(path_csv)
-
-with h5py.File(path_h5) as file:
-    t_end = file.attrs["t_end"]
-    node = file.attrs["node"]
-    nb_particles_short = file.attrs["nb_particles_short"]
-    time_julia_bench = file.attrs["time_julia_bench"]
-    t_sleep_before = file.attrs["t_sleep_before"]
-
-    times = file["times"][:]
-    watts = file["watts"][:]
-
-power_sleep = np.percentile(watts, 5)
-nb_cores = 12
-power_sleep_1core = power_sleep / nb_cores
-
-# fig, ax0 = plt.subplots()
-# ax0.plot(times, watts)
-
-consommations = np.empty(df.shape[0])
-
-deltat = 0.2
-for index, row in df.iterrows():
-
-    cond = (times > row.timestamp_start - deltat) & (
-        times < row.timestamp_end + deltat
-    )
-
-    # ax0.vlines(row.timestamp_start, 0, 250, colors="g")
-    # ax0.vlines(row.timestamp_end, 0, 250, colors="r")
-
-    times_run = times[cond]
-    watts_run = watts[cond]
-
-    # fig, ax0 = plt.subplots()
-    # ax0.plot(times_run, watts_run)
-    # ax0.set_title(row.implementation)
-
-    consommations[index] = trapz(watts_run, times_run)  # in J
-
-
-df["consommation"] = consommations
-df["power"] = df["consommation"] / df["elapsed_time"]
-
+info, df = load_data(path_h5)
 # print(df)
+
+t_end = info["t_end"]
 
 columns = [
     "implementation",
@@ -82,36 +33,26 @@ columns = [
 df_out = df.loc[:, columns].groupby(["implementation", "language"]).mean()
 df_out.drop("sleep(t_sleep_before)", inplace=True)
 
+complete_df_out(df_out, info)
 print(df_out)
 
-elapsed_max = df["elapsed_time"].max()
+# elapsed_max = df["elapsed_time"].max()
 
-df_out["consommation_core"] = df_out["consommation"] - df_out[
-    "elapsed_time"
-] * power_sleep_1core * (nb_cores - 1)
-
-
-df_out["consommation_alt"] = (
-    df_out["consommation"] + (elapsed_max - df_out["elapsed_time"]) * power_sleep
-)
+# df_out["consommation_core"] = df_out["consommation"] - df_out[
+#     "elapsed_time"
+# ] * info["power_sleep_1core"] * (info["nb_cores"] - 1)
 
 
-def compute_CO2_mass(consommation):
-    """
-    kWh = 3.6e6 J
-    0.283 kWh -> 1 kg
+# df_out["consommation_alt"] = (
+#     df_out["consommation"]
+#     + (elapsed_max - df_out["elapsed_time"]) * info["power_sleep"]
+# )
 
-    Also takes into account difference in t_end
-    """
-
-    return consommation * (10 / t_end) / 3.6e6 / 0.283
-
-
-df_out["CO2"] = compute_CO2_mass(df_out["consommation"])
-df_out["CO2_alt"] = compute_CO2_mass(df_out["consommation_alt"])
-df_out["CO2_core"] = compute_CO2_mass(df_out["consommation_core"])
-# in days
-df_out.elapsed_time *= 10 / t_end / (24 * 3600)
+# df_out["CO2"] = (10 / t_end) * compute_CO2_mass(df_out["consommation"])
+# df_out["CO2_alt"] = (10 / t_end) * compute_CO2_mass(df_out["consommation_alt"])
+# df_out["CO2_core"] = (10 / t_end) * compute_CO2_mass(df_out["consommation_core"])
+# # in days
+# df_out.elapsed_time *= 10 / t_end / (24 * 3600)
 
 fig, ax0 = plt.subplots()
 fig, ax1 = plt.subplots()
---
+++ power/make_fig_parallel_julia.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+from pprint import pprint
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+from util import load_data, complete_df_out
+
+here = Path(__file__).absolute().parent
+
+dir_saved = here / "saved"
+
+nb_particles_short = "16k"
+
+paths_h5 = sorted(dir_saved.glob(f"parallel_julia_{nb_particles_short}_*.h5"))
+pprint(paths_h5)
+
+path_h5 = paths_h5[0]
+
+info, df = load_data(path_h5)
+# print(df)
+
+t_end = info["t_end"]
+
+df = df[df.implementation != "sleep(t_sleep_before)"]
+
+print(df.columns)
+
+columns = [
+    "implementation",
+    "language",
+    "nb_threads",
+    "elapsed_time",
+    "consommation",
+    "power",
+]
+
+df_out = df.loc[:, columns].groupby(["nb_threads"]).mean()
+complete_df_out(df_out, info)
+
+print(df_out)
+
+def set_ylim_twin(ax):
+    y0, y1 = ax.get_ylim()
+    ax_twin = ax.ax_twin
+    norm = ax.ax_twin_norm
+    ax_twin.set_ylim(100 * y0 / norm, 100 * y1 / norm)
+    ax_twin.figure.canvas.draw()
+    ax_twin.yaxis.set_major_formatter(mtick.PercentFormatter())
+
+fig, (ax0, ax1) = plt.subplots(2, 1, sharex=True)
+
+nb_threads_list = df_out.index
+
+ax0.plot(nb_threads_list, df_out.elapsed_time, marker=".")
+
+ax0.ax_twin = ax0.twinx()
+ax0.ax_twin_norm = df_out.elapsed_time.max()
+ax0.callbacks.connect("ylim_changed", set_ylim_twin)
+
+
+ax1.plot(nb_threads_list, df_out.CO2, marker=".", label="CO$_2$ during run")
+ax1.plot(
+    nb_threads_list,
+    df_out.CO2_alt,
+    marker=".",
+    label="CO$_2$ during time longer run",
+)
+
+ax1.ax_twin = ax1.twinx()
+ax1.ax_twin_norm = df_out.CO2.max()
+ax1.callbacks.connect("ylim_changed", set_ylim_twin)
+
+ax0.set_ylabel("Elapsed time (day)")
+
+for ax in (ax0, ax1):
+    ax.set_ylim(ymin=0)
+    ax.set_xlim(xmax=12)
+
+ax1.set_xlabel("number of threads")
+ax1.set_ylabel("Production CO$_2$ (kg)")
+ax1.legend()
+ax0.set_title(f"{info['nb_particles_short']} particles, 10 N-Body time units")
+fig.tight_layout()
+
+plt.show()
