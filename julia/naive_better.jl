e671f6b32f2bf28b0627be28c6a90ef66ab9c1d1
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-02-04 21:05:19 +0100

    Again naive_better.jl

--- julia/naive_better.jl
+++ julia/naive_better.jl
@@ -6,7 +6,7 @@ using DelimitedFiles
 tbeg = 0.0
 time_step = 0.001
 
-function NBabel(fname::String; tend = 10.0, show=false)
+function NBabel(fname::String; tend=10.0, show=false)
     if show
 	    println("Reading file : $fname")
     end
@@ -16,7 +16,7 @@ function NBabel(fname::String; tend = 10.0, show=false)
 end
 
 function advance_positions(positions, velocities, accelerations, time_step)
-    @. positions += time_step * velocities + 0.5 * time_step ^ 2 * accelerations
+    @. positions += time_step * velocities + 0.5 * time_step^2 * accelerations
 end
 
 function advance_velocities(velocities, accelerations, accelerations1, time_step)
@@ -26,16 +26,16 @@ end
 function compute_accelerations(accelerations, masses, positions)
     nb_particules = length(masses)
     vector = [0.0, 0.0, 0.0]
-    @inbounds for index_p0=1:nb_particules - 1
-        @fastmath @inbounds @simd for index_p1= index_p0 + 1:nb_particules
+    @inbounds for index_p0 = 1:nb_particules - 1
+        @fastmath @inbounds @simd for index_p1 = index_p0 + 1:nb_particules
             # we need to be a bit more low level otherwise it is very slow
-            # vector[:] = positions[:, index_p0] - positions[:, index_p1]
-            for i in 1:3
-                vector[i] = positions[i, index_p0] - positions[i, index_p1]
-            end
-            distance = sqrt(sum(vector .^ 2))
+            vector[:] = positions[:, index_p0] - positions[:, index_p1]
+            # for i in 1:3
+            #     vector[i] = positions[i, index_p0] - positions[i, index_p1]
+            # end
+            distance = sqrt(sum(vector.^2))
             # distance = sqrt(vector[1] ^ 2 + vector[2] ^ 2 + vector[3] ^ 2)
-            coef = 1.0 / distance ^ 3
+            coef = 1.0 / distance^3
             # accelerations[:, index_p0] -= coef * masses[index_p1] * vector
             # accelerations[:, index_p1] += coef * masses[index_p0] * vector
             for i in 1:3
@@ -56,11 +56,14 @@ function loop(time_step, nb_steps, masses, positions, velocities)
 
     compute_accelerations(accelerations, masses, positions)
 
+    time_start = time_ns()
+
     time = 0.0
     energy0, _, _ = compute_energies(masses, positions, velocities)
     energy_previous = energy0
     energy = energy0
-    for step= 0:nb_steps-1
+    step = 0
+    for step = 0:nb_steps - 1
         advance_positions(positions, velocities, accelerations, time_step)
         # swap acceleration arrays
         accelerations, accelerations1 = accelerations1, accelerations
@@ -78,6 +81,14 @@ function loop(time_step, nb_steps, masses, positions, velocities)
             energy_previous = energy
         end
     end
+    energy, _, _ = compute_energies(masses, positions, velocities)
+    dE = (energy - energy_previous) / energy_previous
+
+    @printf "Final t = %g, Etot=%g, dE/E=%g \n" time_step * step energy dE
+
+    elapsed_time = (time_ns() - time_start) * 1e-9
+    print("$nb_steps time steps run in $elapsed_time s\n")
+
     return energy, energy0
 end
 
@@ -89,12 +100,12 @@ end
 function compute_potential_energy(masses, positions)
     nb_particules = length(masses)
     pe = 0.0
-    @inbounds for index_p0=1:nb_particules - 1
-        @fastmath @inbounds @simd for index_p1= index_p0 + 1:nb_particules
+    @inbounds for index_p0 = 1:nb_particules - 1
+        @fastmath @inbounds @simd for index_p1 = index_p0 + 1:nb_particules
             mass0 = masses[index_p0]
             mass1 = masses[index_p1]
             vector = positions[:, index_p0] - positions[:, index_p1]
-            distance = sqrt(sum(vector .^ 2))
+            distance = sqrt(sum(vector.^2))
             pe -= (mass0 * mass1) / distance
         end
     end
@@ -111,9 +122,9 @@ end
 
 function read_ICs(fname::String)
 
-    ICs = readdlm(fname)
+    ICs = @time readdlm(fname)
 
-    N = size(ICs,1)
+    N = size(ICs, 1)
 
     id = Array{Float64}(undef, N)
     mass = Array{Float64}(undef, N)
